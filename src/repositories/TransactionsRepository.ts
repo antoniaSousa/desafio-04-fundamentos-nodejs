1e8f51bf9873e67ad0122ce5457dd2381d8794d2
antonia <antonia@unifesspa.edu.br>
2020-09-04 11:14:24 -0300

    Finalizando desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (valueGrouper: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            valueGrouper.income += transaction.value;
+            break;
+          case 'outcome':
+            valueGrouper.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return valueGrouper;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
